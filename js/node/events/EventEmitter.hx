334af4043c7687fa79def7ca687496c31c556517
Dan Korostelev <nadako@gmail.com>
2014-10-06 15:44:17 +0400

    Complete and clean up EventEmitter API

--- js/node/events/EventEmitter.hx
+++ js/node/events/EventEmitter.hx
@@ -1,10 +1,7 @@
 package js.node.events;
 
-import js.html.ArrayBufferView;
+import haxe.Constraints.Function;
 
-/**
- * 
- */
 class EventEmitterEventType
 {
 	/**
@@ -13,59 +10,79 @@ class EventEmitterEventType
 	 * This event is emitted any time someone adds a new listener. It is unspecified if listener is in the list returned by emitter.listeners(event).
 	 */
 	static public var NewListener : String = "newListener";
-	
+
 	/**
 	 * event String The event name
 	 * listener Function The event handler function
 	 * This event is emitted any time someone removes a listener. It is unspecified if listener is in the list returned by emitter.listeners(event).
 	 */
 	static public var RemoveListener : String = "removeListener";
-	
+
 }
 
 /**
- * To access the EventEmitter class, require('events').EventEmitter.
- * When an EventEmitter instance experiences an error, the typical action is to emit an 'error' event. Error events are treated as a special case in node.
- * If there is no listener for it, then the default action is to print a stack trace and exit the program.
- * All EventEmitters emit the event 'newListener' when new listeners are added and 'removeListener' when a listener is removed.
- * @author Eduardo Pons - eduardo@thelaborat.org
- */
-@:native("(require('events').EventEmitter)")
-extern class EventEmitter
-{	
-	
+	All objects which emit events are instances of `EventEmitter`.
+
+	Typically, event names are represented by a camel-cased string, however,
+	there aren't any strict restrictions on that, as any string will be accepted.
+
+	Functions can then be attached to objects, to be executed when an event is emitted.
+	These functions are called listeners.
+
+	When an EventEmitter instance experiences an error, the typical action is to emit an 'error' event. Error events are treated as a special case in node.
+	If there is no listener for it, then the default action is to print a stack trace and exit the program.
+
+	All EventEmitters emit the event `newListener` when new listeners are added and `removeListener` when a listener is removed.
+**/
+@:jsRequire("events", "EventEmitter")
+extern class EventEmitter {
+
+	function new();
+
 	/**
-	 * Return the number of listeners for a given event.
-	 * @param	emitter
-	 * @param	event
-	 */
-	static function listenerCount(emitter : EventEmitter, event : String) : Int;
-	
+		Adds a listener to the end of the listeners array for the specified event.
+	**/
+	function addListener(event:String, listener:Function):EventEmitter;
+	function on(event:String, listener:Function):EventEmitter;
+
 	/**
-	 * Adds a listener to the end of the listeners array for the specified event.
-	 * @param	event
-	 * @param	listener
-	 */
-	function on(event : String, listener : Dynamic):EventEmitter;
-	
+		Adds a one time listener for the event.
+		This listener is invoked only the next time the event is fired, after which it is removed.
+	**/
+	function once(event:String, listener:Function):EventEmitter;
+
 	/**
-	 * Adds a one time listener for the event. This listener is invoked only the next time the event is fired, after which it is removed.
-	 * @param	event
-	 * @param	listener
-	 */
-	function once(event : String, listener : Dynamic) : EventEmitter;
-	
+		Remove a listener from the listener array for the specified event.
+		Caution: changes array indices in the listener array behind the listener.
+	**/
+	function removeListener(event:String, listener:Function):EventEmitter;
+
 	/**
-	 * Returns an array of listeners for the specified event.
-	 * @param	event
-	 * @return
-	 */
-	function listeners(event:String):Array<Dynamic>;
-	
+		Removes all listeners, or those of the specified event.
+	**/
+	function removeAllListeners(?event:String):EventEmitter;
+
 	/**
-	 * Removes all listeners, or those of the specified event.
-	 * Returns emitter, so calls can be chained.
-	 */
-	@:overload(function():EventEmitter{})
-	function removeAllListeners(event:String):EventEmitter;
+		By default EventEmitters will print a warning if more than 10 listeners are added for a particular event.
+		This is a useful default which helps finding memory leaks.
+		Obviously not all Emitters should be limited to 10. This function allows that to be increased.
+		Set to zero for unlimited.
+	**/
+	function setMaxListeners(n:Int):EventEmitter;
+
+	/**
+		Returns an array of listeners for the specified event.
+	**/
+	function listeners(event:String):Array<Function>;
+
+	/**
+		Execute each of the listeners in order with the supplied arguments.
+		Returns true if event had listeners, false otherwise.
+	**/
+	function emit(event:String, args:haxe.Rest<Dynamic>):Bool;
+
+	/**
+		Return the number of listeners for a given event.
+	**/
+	static function listenerCount(emitter:EventEmitter, event:String):Int;
 }
