5cbc3722175e36057a32c1c9bca179b5fab7b4a0
Dan Korostelev <nadako@gmail.com>
2014-10-07 18:16:14 +0400

    cleanup string_decoder

--- js/node/StringDecoder.hx
+++ js/node/StringDecoder.hx
@@ -1,31 +1,23 @@
 package js.node;
 
 /**
- * To use this module, do require('string_decoder'). StringDecoder decodes a buffer to a string. It is a simple interface to buffer.toString() but provides additional support for utf8.
- * @author Eduardo Pons - eduardo@thelaborat.org
- */
-@:native("(require('string_decoder'))")
-extern class StringDecoder
-{
-	
+	StringDecoder decodes a buffer to a string.
+	It is a simple interface to buffer.toString() but provides additional support for utf8.
+**/
+@:jsRequire("string_decoder", "StringDecoder")
+extern class StringDecoder {
 	/**
-	 * Accepts a single argument, encoding which defaults to utf8.
-	 * @param	encoding
-	 */
-	@:overload(function():Void { } )	
-	function new(encoding:String):Void;
+		`encoding` defaults to `utf8`
+	**/
+	function new(?encoding:String);
 
 	/**
-	 * Returns a decoded string.
-	 * @param	buffer
-	 * @return
-	 */
+		Returns a decoded string.
+	**/
 	function write(buffer:Buffer):String;
-	
+
 	/**
-	 * Returns any trailing bytes that were left in the buffer.
-	 * @return
-	 */
-	function end():Buffer;
-	
+		Returns any trailing bytes that were left in the buffer.
+	**/
+	function end():String;
 }
