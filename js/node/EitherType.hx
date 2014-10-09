0161170dff1c3804fda2704e6ebfad3b6be0d2bc
Dan Korostelev <nadako@gmail.com>
2014-10-10 00:59:24 +0400

    hmm, not sure

--- js/node/EitherType.hx
+++ js/node/EitherType.hx
@@ -5,5 +5,5 @@ package js.node;
 
     TODO: move it into std lib? (see https://github.com/HaxeFoundation/hxnodejs/issues/15)
 **/
-@:forward
+@:forward // TODO: is this a good idea?
 abstract EitherType<T1,T2>(Dynamic) from T1 to T1 from T2 to T2 {}
