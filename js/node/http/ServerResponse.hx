bd72cd7f0bb33c3e528e1af6d47370f076a49f13
Dan Korostelev <nadako@gmail.com>
2014-10-11 03:52:43 +0400

    http.ServerResponse API

--- js/node/http/ServerResponse.hx
+++ js/node/http/ServerResponse.hx
@@ -1,81 +1,112 @@
 package js.node.http;
-import js.html.ArrayBufferView;
-import js.node.fs.WriteStream;
+
+import js.node.stream.Writable;
 
 /**
- * 
- */
-class ServerResponseEventType
-{
+	Enumeration of events emitted by the `ServerResponse` objects.
+**/
+@:enum abstract ServerResponseEvent(String) to String {
 	/**
-	 * function () { }
-	 * Indicates that the underlying connection was terminated before response.end() was called or able to flush.
-	 */
-	static public var Close : String = "close";
-	
-	
-	
+		Indicates that the underlying connection was terminated before `end` was called or able to flush.
+	**/
+	var Close = "close";
+
 	/**
-	 * function () { }
-	 * Emitted when the response has been sent. More specifically, this event is emitted when the last segment of the response headers and body have been handed off to the operating system for transmission over the network. It does not imply that the client has received anything yet.
-	 * After this event, no more events will be emitted on the response object.
-	 */
-	static public var Finish : String = "finish";
+		Emitted when the response has been sent. More specifically, this event is emitted when
+		the last segment of the response headers and body have been handed off to the operating system
+		for transmission over the network. It does not imply that the client has received anything yet.
+
+		After this event, no more events will be emitted on the response object.
+	**/
+	var Finish = "finish";
 }
 
 /**
- * This object is created internally by a HTTP server--not by the user. It is passed as the second parameter to the 'request' event.
- * The response implements the Writable Stream interface. This is an EventEmitter with the following events:
- * @author Eduardo Pons - eduardo@thelaborat.org
- */
-extern class ServerResponse extends WriteStream
-{	
-	
+	This object is created internally by a HTTP server--not by the user.
+	It is passed as the second parameter to the 'request' event.
+**/
+extern class ServerResponse extends Writable {
+
 	/**
-	 * Sends a HTTP/1.1 100 Continue message to the client, indicating that the request body should be sent. See the 'checkContinue' event on Server.
+		Sends a HTTP/1.1 100 Continue message to the client, indicating that the request body should be sent.
+		See the 'checkContinue' event on `Server`.
 	 */
 	function writeContinue():Void;
-	
+
 	/**
-	 * Sends a response header to the request. The status code is a 3-digit HTTP status code, like 404.
-	 * The last argument, headers, are the response headers. Optionally one can give a human-readable reasonPhrase as the second argument.
-	 * This method must only be called once on a message and it must be called before response.end() is called.
-	 * If you call response.write() or response.end() before calling this, the implicit/mutable headers will be calculated and call this function for you.
-	 * Note: that Content-Length is given in bytes not characters. 
-	 */
-	@:overload( function( statusCode : Int, headers : Dynamic ) :Void { } )
-	@:overload( function( statusCode : Int) :Void { } )	
-	function writeHead(statusCode : Int, reasonPhrase : String, headers : Dynamic) : Void;
-	
+		Sends a response header to the request.
+		The status code is a 3-digit HTTP status code, like 404.
+		The last argument, `headers`, are the response headers.
+		Optionally one can give a human-readable `reasonPhrase` as the second argument.
+
+		This method must only be called once on a message and it must be called before `end` is called.
+
+		If you call `write` or `end` before calling this, the implicit/mutable headers
+		will be calculated and call this function for you.
+
+		Note: that Content-Length is given in bytes not characters.
+	**/
+	@:overload(function(statusCode:Int, ?headers:Dynamic<String>):Void {})
+	function writeHead(statusCode:Int, reasonPhrase:String, ?headers:Dynamic<String>):Void;
+
 	/**
-	 * When using implicit headers (not calling response.writeHead() explicitly), this property controls the status code that will be sent to the client when the headers get flushed.
-	 */
-	var statusCode : Int;
-	
+		Sets the `Socket`'s timeout value to `msecs`.
+		If a `callback` is provided, then it is added as a listener on the 'timeout' event on the response object.
+
+		If no 'timeout' listener is added to the request, the response, or the server,
+		then sockets are destroyed when they time out. If you assign a handler on the request,
+		the response, or the server's 'timeout' events, then it is your responsibility to handle timed out sockets.
+	**/
+	function setTimeout(msecs:Int, ?callback:Void->Void):Void;
+
 	/**
-	 * Boolean (read-only). True if headers were sent, false otherwise.
-	 */
-	var headersSent : Bool;
-	 
-	 /**
-	  * Reads out a header that's already been queued but not sent to the client. Note that the name is case insensitive. This can only be called before headers get implicitly flushed.
-	  * @param	name
-	  * @return
-	  */
-	 function getHeader(name : String):String;
-	 
-	  /**
-	   * Sets a single header value for implicit headers. If this header already exists in the to-be-sent headers, its value will be replaced. Use an array of strings here if you need to send multiple headers with the same name.
-	   * @param	name
-	   * @param	value
-	   */
-	 function setHeader(name : String, value : String) : Void;
-	 
-	/*	 
-    setTimeout(msecs, callback)            
-    sendDate    
-    removeHeader(name)    
-    addTrailers(headers)    
-	//*/
-	 
+		When using implicit headers (not calling `writeHead` explicitly), this property controls the status code
+		that will be sent to the client when the headers get flushed.
+	**/
+	var statusCode:Int;
+
+	/**
+		True if headers were sent, false otherwise.
+	**/
+	var headersSent(default,null):Bool;
+
+	/**
+		Reads out a header that's already been queued but not sent to the client.
+		Note that the name is case insensitive.
+		This can only be called before headers get implicitly flushed.
+	**/
+	function getHeader(name:String):String;
+
+	/**
+		Sets a single header value for implicit headers.
+		If this header already exists in the to-be-sent headers, its value will be replaced.
+		Use an array of strings here if you need to send multiple headers with the same name.
+	**/
+	@:overload(function(name:String, value:Array<String>):Void {})
+	function setHeader(name:String, value:String):Void;
+
+	/**
+		Removes a header that's queued for implicit sending.
+	**/
+	function removeHeader(name:String):Void;
+
+	/**
+		When true, the Date header will be automatically generated and sent in the response
+		if it is not already present in the headers.
+		Defaults to true.
+
+		This should only be disabled for testing; HTTP requires the Date header in responses.
+	**/
+	var sendDate:Bool;
+
+	/**
+		This method adds HTTP trailing headers (a header but at the end of the message) to the response.
+
+		Trailers will only be emitted if chunked encoding is used for the response;
+		if it is not (e.g., if the request was HTTP/1.0), they will be silently discarded.
+
+		Note that HTTP requires the 'Trailer' header to be sent if you intend to emit trailers,
+		with a list of the header fields in its value.
+	**/
+    function addTrailers(headers:Dynamic<String>):Void;
 }
