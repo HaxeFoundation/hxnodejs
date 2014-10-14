15a3163311f28a80a44ad1540f8a740ca4bcf8b7
Dan Korostelev <nadako@gmail.com>
2014-10-14 14:45:33 +0400

    fix http.ClientRequest

--- js/node/http/ClientRequest.hx
+++ js/node/http/ClientRequest.hx
@@ -15,6 +15,8 @@ import js.node.stream.Writable;
 
 	/**
 		Emitted after a socket is assigned to this request.
+		Listener arguments:
+			* socket : Socket
 	**/
 	var Socket = "socket";
 
@@ -52,7 +54,25 @@ import js.node.stream.Writable;
 
 /**
 	This object is created internally and returned from `Http.request`.
+
 	It represents an in-progress request whose header has already been queued.
+	The header is still mutable using the `setHeader`, `getHeader` and `removeHeader`.
+	The actual header will be sent along with the first data chunk or when closing the connection.
+
+	To get the response, add a listener for 'response' to the request object.
+	'response' will be emitted from the request object when the response headers have been received.
+	The 'response' event is executed with one argument which is an instance of `IncomingMessage`.
+
+	During the 'response' event, one can add listeners to the response object;
+	particularly to listen for the 'data' event.
+
+	If no 'response' handler is added, then the response will be entirely discarded.
+	However, if you add a 'response' event handler, then you must consume the data from the response object,
+	either by calling `read` whenever there is a 'readable' event, or by adding a 'data' handler, or by calling
+	the `resume` method. Until the data is consumed, the 'end' event will not fire. Also, until the data is read
+	it will consume memory that can eventually lead to a 'process out of memory' error.
+
+	Note: Node does not check whether 'Content-Length' and the length of the body which has been transmitted are equal or not.
 **/
 extern class ClientRequest extends Writable {
 
@@ -97,5 +117,5 @@ extern class ClientRequest extends Writable {
 		`socket.setKeepAlive`() will be called.
 	**/
     @:overload(function(?initialDelay:Int):Void {})
-    function setKeepAlive(enable:Bool, ?initialDelay:Int):Void;
+    function setSocketKeepAlive(enable:Bool, ?initialDelay:Int):Void;
 }
