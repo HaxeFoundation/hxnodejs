5734d0d2d6a4b74b8fca0e1ecc166fa089f51eb4
Dan Korostelev <nadako@gmail.com>
2014-10-18 03:55:37 +0400

    minor

--- examples/AssertThrows.hx
+++ examples/AssertThrows.hx
@@ -7,12 +7,7 @@ class AssertThrows {
 		Assert.throws(function() throw new Error("Wrong value"), new js.RegExp("value"));
 		Assert.throws(
 			function() throw new Error("Wrong value"),
-			function(err) {
-				if (Std.is(err, Error) && ~/value/.match(err.message))
-					return true;
-				else
-					return false;
-			},
+			function(err) return (Std.is(err, Error) && ~/value/.match(err.message)),
 			"unexpected error"
 		);
 	}
