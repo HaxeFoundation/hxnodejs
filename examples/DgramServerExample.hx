e868c83434a2d89bff67036e75d9b1ff3173db3b
Dan Korostelev <nadako@gmail.com>
2014-10-17 20:01:58 +0400

    add dgram server example

---
+++ examples/DgramServerExample.hx
@@ -0,0 +1,24 @@
+import js.node.Dgram;
+import js.node.NodeJS.console;
+
+class DgramServerExample {
+	static function main() {
+		var server = Dgram.createSocket("udp4");
+
+		server.on("error", function (err) {
+			console.log("server error:\n" + err.stack);
+			server.close();
+		});
+
+		server.on("message", function (msg, rinfo) {
+			console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
+		});
+
+		server.on("listening", function () {
+			var address = server.address();
+			console.log("server listening " + address.address + ":" + address.port);
+		});
+
+		server.bind(41234);
+	}
+}
