fc655ce1938528c18e4c37c565fccb6a012cf0b1
Dan Korostelev <nadako@gmail.com>
2014-10-19 15:30:57 +0400

    repl example

---
+++ examples/ReplExample.hx
@@ -0,0 +1,40 @@
+import js.node.Net;
+import js.node.Repl;
+import js.node.NodeJS.process;
+
+/**
+	Example from the REPL page
+**/
+class ReplExample {
+    static function main() {
+		var connections = 0;
+
+		Repl.start({
+			prompt: "node via stdin> ",
+			input: process.stdin,
+			output: process.stdout
+		});
+
+		Net.createServer(function (socket) {
+			connections += 1;
+			Repl.start({
+				prompt: "node via Unix socket> ",
+				input: socket,
+				output: socket
+			}).on('exit', function() {
+				socket.end();
+			});
+		}).listen("/tmp/node-repl-sock");
+
+		Net.createServer(function (socket) {
+			connections += 1;
+			Repl.start({
+				prompt: "node via TCP socket> ",
+				input: socket,
+				output: socket
+			}).on('exit', function() {
+				socket.end();
+			});
+		}).listen(5001);
+    }
+}
