18a60bca4885f74f5085e22f266b4bb4f491fcf1
Dan Korostelev <nadako@gmail.com>
2014-10-19 18:10:03 +0400

    cleanup tls.SecurePair

--- js/node/tls/SecurePair.hx
+++ js/node/tls/SecurePair.hx
@@ -1,21 +1,25 @@
 package js.node.tls;
 
+import js.node.events.EventEmitter;
+
 /**
 	Events emitted by `SecurePair`.
 **/
-@:enum abstract SecurePairEvent(String) to String {
+@:enum abstract SecurePairEvent<T:haxe.Constraints.Function>(Event<T>) to Event<T> {
 	/**
 		The event is emitted from the `SecurePair` once the pair has successfully established a secure connection.
-		Similarly to the checking for the server 'secureConnection' event, pair.cleartext.authorized should be checked
-		to confirm whether the certificate used properly authorized.
+
+		Similarly to the checking for the server 'secureConnection' event,
+		`SecurePair.cleartext.authorized` should be checked to confirm whether
+		the certificate used properly authorized.
 	**/
-	var Secure = "secure";
+	var Secure : SecurePairEvent<Void->Void> = "secure";
 }
 
 /**
 	Returned by `Tls.createSecurePair`.
 **/
-extern class SecurePair extends js.node.events.EventEmitter<SecurePair> {
-	var cleartext:CleartextStream;
-	var encrypted:EncryptedStream;
+extern class SecurePair extends EventEmitter<SecurePair> {
+	var cleartext(default,null):CleartextStream;
+	var encrypted(default,null):EncryptedStream;
 }
