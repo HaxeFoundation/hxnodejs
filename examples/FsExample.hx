9eca09ccc57c45fe608abdec64aa72f44ed29750
Dan Korostelev <nadako@gmail.com>
2014-10-19 18:05:10 +0400

    fs example

---
+++ examples/FsExample.hx
@@ -0,0 +1,17 @@
+import js.node.Fs;
+import js.node.NodeJS.console;
+
+/**
+	Example from fs API page.
+**/
+class FsExample {
+	static function main() {
+		Fs.rename('/tmp/hello', '/tmp/world', function (err) {
+			if (err != null) throw err;
+			Fs.stat('/tmp/world', function (err, stats) {
+				if (err != null) throw err;
+				console.log('stats: ' + haxe.Json.stringify(stats));
+			});
+		});
+	}
+}
