240e8a776710fbddbfc4c0590159e7b26ca7e43c
Dan Korostelev <nadako@gmail.com>
2014-10-20 23:46:16 +0400

    minor cleanup

--- js/node/Module.hx
+++ js/node/Module.hx
@@ -1,7 +1,5 @@
 package js.node;
 
-import haxe.DynamicAccess;
-
 /**
 	In each module, the `module` free variable is a reference to the object representing the current module.
 	For convenience, `module.exports` is also accessible via the `exports` module-global.
@@ -15,15 +13,15 @@ extern class Module {
 		To do this assign the desired export object to module.exports.
 		Note that assigning the desired object to `exports` will simply rebind the local exports variable, which is probably not what you want to do.
 	**/
-	var exports:DynamicAccess<Dynamic>;
+	var exports:Dynamic<Dynamic>;
 
 	/**
-		Return `exports` from the resolved module
+		Return `exports` from the resolved module.
 		The `require` method provides a way to load a module as if `require` was called from the original module.
 		Note that in order to do this, you must get a reference to the `module` object. Since `require` returns the `module.exports`,
 		and the `module` is typically only available within a specific module's code, it must be explicitly exported in order to be used.
 	**/
-	function require(id:String):DynamicAccess<Dynamic>;
+	function require(id:String):Dynamic<Dynamic>;
 
 	/**
 		The identifier for the module.
