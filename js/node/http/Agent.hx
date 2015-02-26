2173f8a5314f057d1b09ef9d11ed5196a9629b29
Dan Korostelev <nadako@gmail.com>
2015-02-26 03:20:43 +0300

    http.Agent 0.12

--- js/node/http/Agent.hx
+++ js/node/http/Agent.hx
@@ -28,6 +28,13 @@ extern class Agent {
 	var maxSockets:Float;
 
 	/**
+		For Agents supporting HTTP KeepAlive, this sets the maximum number of sockets
+		that will be left open in the free state.
+		Default: 256
+	**/
+	var maxFreeSockets:Float;
+
+	/**
 		An object which contains arrays of sockets currently in use by the Agent.
 		Do not modify.
 	**/
@@ -38,4 +45,56 @@ extern class Agent {
 		Do not modify.
 	**/
 	var requests(default,null):DynamicAccess<Array<ClientRequest>>;
+
+	function new(?options:AgentOptions);
+
+	/**
+		Destroy any sockets that are currently in use by the agent.
+
+		It is usually not necessary to do this. However, if you are using an agent with KeepAlive enabled,
+		then it is best to explicitly shut down the agent when you know that it will no longer be used.
+		Otherwise, sockets may hang open for quite a long time before the server terminates them.
+	**/
+	function destroy():Void;
+
+	/**
+		Get a unique name for a set of request options, to determine whether a connection can be reused.
+		In the http agent, this returns host:port:localAddress. In the https agent, the name includes the CA,
+		cert, ciphers, and other HTTPS/TLS-specific options that determine socket reusability.
+
+		TODO: proper typing for this?
+	**/
+	function getName(options:{}):String;
+}
+
+
+/**
+	Options for `Agent` constructor.
+**/
+typedef AgentOptions = {
+	/**
+		Keep sockets around in a pool to be used by other requests in the future.
+		Default: false
+	**/
+	@:optional var keepAlive:Bool;
+
+	/**
+		When using HTTP KeepAlive, how often to send TCP KeepAlive packets over sockets being kept alive.
+		Default: 1000.
+		Only relevant if `keepAlive` is set to `true`.
+	**/
+	@:optional var keepAliveMsecs:Int;
+
+	/**
+		Maximum number of sockets to allow per host.
+		Default: Infinity.
+	**/
+	@:optional var maxSockets:Float;
+
+	/**
+		Maximum number of sockets to leave open in a free state.
+		Only relevant if `keepAlive` is set to `true`.
+		Default: 256.
+	**/
+	@:optional var maxFreeSockets:Float;
 }
