871614b85586d2327df3a307f9ef7bb14c8b8281
Dan Korostelev <nadako@gmail.com>
2015-09-09 14:15:54 +0300

    move hx files into src folder as we not only have "js" package now but also "sys" and "Sys.hx" in the root

--- BuildExamples.hx
+++ BuildExamples.hx
@@ -2,14 +2,14 @@ import haxe.io.Path;
 import sys.FileSystem;
 
 class BuildExamples {
-    static function run() {
+    static function main() {
         Sys.setCwd("examples");
         for (file in FileSystem.readDirectory(".")) {
             if (Path.extension(file) == "hx") {
                 var main = Path.withoutExtension(file);
                 Sys.command("haxe", [
                     "-main", main,
-                    "-cp", "..",
+                    "-cp", "../src",
                     "-js", '$main.js',
                     "-D", "js-es5",
                     "-D", "analyzer",
--- ImportAll.hx
+++ ImportAll.hx
@@ -1,20 +1,26 @@
 import haxe.io.Path;
+import haxe.macro.Compiler;
 import haxe.macro.Context;
 import sys.FileSystem;
 
 class ImportAll {
-    static function run(root:String):Void {
+    static function run(cp:String):Void {
+        Compiler.addClassPath(cp);
+        Compiler.allowPackage("sys");
+        Compiler.define("nodejs");
+
         function loop(acc:Array<String>):Void {
-            var path = Path.join(acc);
+            var path = Path.join([cp].concat(acc));
             if (FileSystem.isDirectory(path)) {
                 for (file in FileSystem.readDirectory(path))
                     loop(acc.concat([file]));
             } else if (Path.extension(path) == "hx") {
                 var moduleName = Path.withoutExtension(acc[acc.length - 1]);
                 var modulePath = acc.slice(0, acc.length - 1);
-                Context.getType(modulePath.join(".") + "." + moduleName);
+                var typePath = if (modulePath.length == 0) moduleName else modulePath.join(".") + "." + moduleName;
+                Context.getType(typePath);
             }
         }
-        loop([root]);
+        loop([]);
     }
 }
