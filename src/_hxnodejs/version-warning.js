3aa09438dd194fdb320e0937ff6577641022ba46
Nicolas Cannasse <ncannasse@gmail.com>
2015-11-23 17:23:08 +0100

    fix for node webkit (module is not defined)

--- src/_hxnodejs/version-warning.js
+++ src/_hxnodejs/version-warning.js
@@ -1,1 +1,1 @@
-if (process.version < "v4.0.0") console.warn("Module " + module.filename + " requires node.js version 4.0.0 or higher");
+if (process.version < "v4.0.0") console.warn("Module " + (typeof(module) == "undefined" ? "" : module.filename) + " requires node.js version 4.0.0 or higher");
