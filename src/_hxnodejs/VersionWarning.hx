1e3fba4b20022f4c885bfce7469cf31679c6f25c
Dan Korostelev <nadako@gmail.com>
2016-02-23 21:24:29 +0300

    don't try including anything in display mode

--- src/_hxnodejs/VersionWarning.hx
+++ src/_hxnodejs/VersionWarning.hx
@@ -3,7 +3,7 @@ package _hxnodejs;
 #if macro
 class VersionWarning {
     static function include() {
-        #if (haxe_ver >= 3.3)
+        #if (!display && haxe_ver >= 3.3)
         if (!haxe.macro.Context.defined("hxnodejs_no_version_warning"))
             haxe.macro.Compiler.includeFile("_hxnodejs/version-warning.js");
         #end
