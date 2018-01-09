2587ccbffa3cc032d7ecca28d299e7357993f536
Dan Korostelev <nadako@gmail.com>
2018-01-09 21:41:22 +0300

    remove ancient node.js support

--- src/_hxnodejs/version-warning.js
+++
@@ -1,1 +0,0 @@
-if (typeof process !== "undefined") if (process.version < "v4.0.0") console.warn("Module " + (typeof(module) == "undefined" ? "" : module.filename) + " requires node.js version 4.0.0 or higher");
