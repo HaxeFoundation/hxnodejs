b5dfad41727406fb3d50a9d75eaa3f47b997651c
takashiski <takashiskibb@gmail.com>
2019-08-28 10:45:32 +0900

    add clear()

--- src/js/node/console/Console.hx
+++ src/js/node/console/Console.hx
@@ -47,7 +47,13 @@ extern class Console {
 	@:overload(function(value:Bool, args:haxe.extern.Rest<Dynamic>):Void {})
 	@:overload(function(value:Bool, message:String, args:haxe.extern.Rest<Dynamic>):Void {})
 	function assert(value:Bool):Void;
+	/**
+		When `stdout` is a TTY, calling `console.clear()` will attempt to clear the TTY. When `stdout` is not a TTY, this method does nothing.
 
+		The specific operation of `console.clear()` can vary across operating systems and terminal types. For most Linux operating systems, `console.clear()` operates similarly to the clear shell command.
+  		On Windows, `console.clear()` will clear only the output in the current terminal viewport for the Node.js binary.
+	**/
+	function clear():Void;
 	/**
 		Uses util.inspect on obj and prints resulting string to stdout.
 
