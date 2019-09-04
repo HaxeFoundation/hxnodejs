11656da428c59db149a97a68da53bf6f731ed58f
takashiski <takashiskibb@gmail.com>
2019-09-04 10:04:11 +0900

    update Querystring.hx to v12 (#145)
    * sort existed items
    
    * add new functions
    
    * fixed miss types
    
    * update functions comment
    
    * update function definitions
    
    * tidy typedefs
    
    * update overlording functions

--- src/js/node/Querystring.hx
+++ src/js/node/Querystring.hx
@@ -26,87 +26,102 @@ import haxe.extern.EitherType;
 import haxe.DynamicAccess;
 
 /**
-	Options used for `Querystring.parse` method.
-**/
-typedef QuerystringParseOptions = {
-	/**
-		Specifies the maximum number of keys to parse.
-		Defaults to 1000.
-		Specify 0 to remove key counting limitations.
-	**/
-	@:optional var maxKeys:Int;
+	The `querystring` module provides utilities for parsing and formatting URL query strings.
 
-	/**
-		The function to use when decoding percent-encoded characters in the query string.
-		Defaults to `Querystring.unescape`.
-	**/
-	@:optional var decodeURIComponent:String->String;
-}
-
-/**
-	Options for `Querystring.stringify` method.
-**/
-typedef QuerystringStringifyOptions = {
-	/**
-		The function to use when converting URL-unsafe characters to percent-encoding in the query string.
-		Defaults to `Querystring.escape`.
-	**/
-	@:optional var encodeURIComponent:String->String;
-}
-
-/**
-	The result type of `Querystring.parse`. Is a collection of either strings or array of strings.
-**/
-typedef QuerystringParseResult = DynamicAccess<EitherType<String, Array<String>>>;
-
-/**
-	This module provides utilities for dealing with query strings.
+	@see https://nodejs.org/api/querystring.html#querystring_query_string
 **/
 @:jsRequire("querystring")
 extern class Querystring {
 	/**
-		Produces a URL query string from a given `obj` by iterating through the object's "own properties".
+		The `querystring.decode()` function is an alias for `querystring.parse()`.
 
-		If `sep` is provided, it is used to delimit key and value pairs in the query string. Defaults to '&'.
-		If `eq` is provided, it is used to delimit keys and values in the query string. Defaults to '='.
-
-		By default, characters requiring percent-encoding within the query string will be encoded as UTF-8.
-		If an alternative encoding is required, then an alternative `encodeURIComponent` option will need to be specified.
+		@see https://nodejs.org/api/querystring.html#querystring_querystring_decode
 	**/
-	@:overload(function(obj:{}, ?sep:String):String {})
-	static function stringify(obj:{}, sep:String, eq:String, ?options:QuerystringStringifyOptions):String;
+	@:overload(function(str:String):QuerystringParseResult {})
+	@:overload(function(str:String, sep:String):QuerystringParseResult {})
+	@:overload(function(str:String, sep:String, eq:String):QuerystringParseResult {})
+	static function decode(str:String, sep:String, eq:String, options:QuerystringParseOptions):QuerystringParseResult;
 
 	/**
-		Parses a URL query string (`str`) into a collection of key and value pairs.
+		The `querystring.encode()` function is an alias for `querystring.stringify()`.
 
-		If `sep` is provided, it is used to delimit key and value pairs in the query string. Defaults to '&'.
-		If `eq` is provided, it is used to delimit keys and values in the query string. Defaults to '='.
-
-		By default, percent-encoded characters within the query string will be assumed to use UTF-8 encoding.
-		If an alternative character encoding is used, then an alternative `decodeURIComponent` option will need to be specified.
+		@see https://nodejs.org/api/querystring.html#querystring_querystring_decode
 	**/
-	@:overload(function(str:String, ?sep:String):QuerystringParseResult {})
-	static function parse(str:String, sep:String, eq:String, ?options:QuerystringParseOptions):QuerystringParseResult;
+	@:overload(function(obj:{}):String {})
+	@:overload(function(obj:{}, sep:String):String {})
+	@:overload(function(obj:{}, sep:String, eq:String):String {})
+	static function encode(obj:{}, sep:String, eq:String, options:QuerystringStringifyOptions):String;
 
 	/**
-		Performs URL percent-encoding on the given `str` in a manner that is optimized for
-		the specific requirements of URL query strings.
+		The `querystring.escape()` method performs URL percent-encoding on the given `str` in a manner that is optimized for the specific requirements of URL query strings.
 
-		This method is used by `stringify` and is generally not expected to be used directly.
-		It is exported primarily to allow application code to provide a replacement percent-encoding implementation
-		if necessary by assigning `Querystring.escape` to an alternative function.
+		@see https://nodejs.org/api/querystring.html#querystring_querystring_escape_str
 	**/
 	static dynamic function escape(str:String):String;
 
 	/**
-		Performs decoding of URL percent-encoded characters on the given `str`.
+		The `querystring.parse()` method parses a URL query string (`str`) into a collection of key and value pairs.
 
-		This method is used by `Querystring.parse` and is generally not expected to be used directly.
-		It is exported primarily to allow application code to provide a replacement decoding implementation
-		if necessary by assigning `Querystring.unescape` to an alternative function.
+		@see https://nodejs.org/api/querystring.html#querystring_querystring_parse_str_sep_eq_options
+	**/
+	@:overload(function(str:String):QuerystringParseResult {})
+	@:overload(function(str:String, sep:String):QuerystringParseResult {})
+	@:overload(function(str:String, sep:String, eq:String):QuerystringParseResult {})
+	static function parse(str:String, sep:String, eq:String, options:QuerystringParseOptions):QuerystringParseResult;
 
-		By default, this method will attempt to use the JavaScript built-in `decodeURIComponent` method to decode.
-		If that fails, a safer equivalent that does not throw on malformed URLs will be used.
+	/**
+		The `querystring.stringify()` method produces a URL query string from a given `obj` by iterating through the object's "own properties".
+
+		@see https://nodejs.org/api/querystring.html#querystring_querystring_stringify_obj_sep_eq_options
+	**/
+	@:overload(function(obj:{}):String {})
+	@:overload(function(obj:{}, sep:String):String {})
+	@:overload(function(obj:{}, sep:String, eq:String):String {})
+	static function stringify(obj:{}, sep:String, eq:String, options:QuerystringStringifyOptions):String;
+
+	/**
+		The `querystring.unescape()` method performs decoding of URL percent-encoded characters on the given `str`.
+
+		@see https://nodejs.org/api/querystring.html#querystring_querystring_unescape_str
 	**/
 	static dynamic function unescape(str:String):Dynamic;
+}
+
+/**
+	Options used for `Querystring.parse` method.
+
+	@see https://nodejs.org/api/querystring.html#querystring_querystring_parse_str_sep_eq_options
+**/
+typedef QuerystringParseOptions = {
+	/**
+		The function to use when decoding percent-encoded characters in the query string. Default: `querystring.unescape()`.
+	**/
+	@:optional var decodeURIComponent:String->String;
+
+	/**
+		Specifies the maximum number of keys to parse. Specify `0` to remove key counting limitations. Default: `1000`.
+	**/
+	@:optional var maxKeys:Int;
+}
+
+/**
+	The result type of `Querystring.parse`. Is a collection of either strings or array of strings.
+
+	The object returned by the `querystring.parse()` method does not prototypically inherit from the JavaScript `Object`.
+	This means that typical `Object` methods such as `obj.toString()`, `obj.hasOwnProperty()`, and others are not defined and will not work.
+
+	@see https://nodejs.org/api/querystring.html#querystring_querystring_parse_str_sep_eq_options
+**/
+typedef QuerystringParseResult = DynamicAccess<EitherType<String, Array<String>>>;
+
+/**
+	Options for `Querystring.stringify` method.
+
+	@see https://nodejs.org/api/querystring.html#querystring_querystring_stringify_obj_sep_eq_options
+**/
+typedef QuerystringStringifyOptions = {
+	/**
+		The function to use when converting URL-unsafe characters to percent-encoding in the query string. Default: `querystring.escape()`.
+	**/
+	@:optional var encodeURIComponent:String->String;
 }
