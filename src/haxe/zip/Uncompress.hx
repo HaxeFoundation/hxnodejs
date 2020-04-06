e7f34b06f387ae2340686e711f8603843efe15e8
P.J.Shand <pete@peteshand.net>
2020-04-06 10:43:00 +1000

    add missing methods which are causing issues in haxe.zip.Reader

--- src/haxe/zip/Uncompress.hx
+++ src/haxe/zip/Uncompress.hx
@@ -1,6 +1,18 @@
 package haxe.zip;
 
 class Uncompress {
+	public function new(?windowBits:Int) {
+		throw "Not implemented for this platform";
+	}
+
+	public function execute(src:haxe.io.Bytes, srcPos:Int, dst:haxe.io.Bytes, dstPos:Int):{done:Bool, read:Int, write:Int} {
+		return null;
+	}
+
+	public function setFlushMode(f:FlushMode) {}
+
+	public function close() {}
+
 	public static function run(src:haxe.io.Bytes, ?bufsize:Int):haxe.io.Bytes {
 		var buffer = js.node.Zlib.inflateSync(js.node.Buffer.hxFromBytes(src), bufsize == null ? {} : {chunkSize: bufsize});
 		return buffer.hxToBytes();
