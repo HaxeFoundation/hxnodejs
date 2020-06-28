9b9b5a66bf6b52b46f4c7aa5d864dd89a12b7935
Cédric Belin <cedric@belin.io>
2020-06-28 18:22:19 +0200

    Make static the instance methods of module `js.node.Os`, fixes #168 (#169)

--- src/js/node/Os.hx
+++ src/js/node/Os.hx
@@ -134,21 +134,21 @@ extern class Os {
 
 		@see https://nodejs.org/api/os.html#os_os_tmpdir
 	**/
-	function tmpdir():String;
+	static function tmpdir():String;
 
 	/**
 		The `os.totalmem()` method returns the total amount of system memory in bytes as an integer.
 
 		@see https://nodejs.org/api/os.html#os_os_totalmem
 	**/
-	function totalmem():Int;
+	static function totalmem():Int;
 
 	/**
 		The `os.type()` method returns a string identifying the operating system name as returned by uname(3). For example, `'Linux'` on Linux, `'Darwin'` on macOS, and `'Windows_NT'` on Windows.
 
 		@see https://nodejs.org/api/os.html#os_os_type
 	**/
-	function type():String;
+	static function type():String;
 
 	/**
 		The `os.uptime()` method returns the system uptime in number of seconds.
